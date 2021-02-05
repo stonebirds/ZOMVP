d26689586ecae777d19f6e7a7c3530dce355c524
GitHub Action <action@github.com>
2021-02-05 06:37:01 +0000

    Template cleanup

--- src/main/kotlin/com/github/stonebirds/zomvp/MyBundle.kt
+++ src/main/kotlin/com/github/stonebirds/zomvp/MyBundle.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template
+package com.github.stonebirds.zomvp
 
 import com.intellij.AbstractBundle
 import org.jetbrains.annotations.NonNls
--- src/main/kotlin/com/github/stonebirds/zomvp/services/MyProjectService.kt
+++ src/main/kotlin/com/github/stonebirds/zomvp/services/MyProjectService.kt
@@ -1,7 +1,7 @@
-package org.jetbrains.plugins.template.services
+package com.github.stonebirds.zomvp.services
 
+import com.github.stonebirds.zomvp.MyBundle
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
 
 class MyProjectService(project: Project) {
 
